cba920988bd765c6985c4264024c19e72ec30ec7
VIVEK BHARDWAJ <bvivek514@gmail.com>
2025-11-07 02:13:43 +0000

    problem day

--- DataStructure/Array/LinkedList/Problem/01_Problem.c
+++ DataStructure/Array/LinkedList/Problem/01_Problem.c
@@ -3,6 +3,131 @@
 
 // Create and display a singly linked list
 
+// struct Node
+// {
+//     int data;
+//     struct Node *next;
+// };
+
+// struct Node *head;
+
+// struct Node *Insert(struct Node *head, int data)
+// {
+//     struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+//     temp->data = data;
+//     temp->next = NULL;
+
+//     if (head == NULL)
+//     {
+//         head = temp;
+//     }
+//     else
+//     {
+//         struct Node *node = head;
+//         while (node->next != NULL)
+//         {
+//             node = node->next;
+//         }
+//         node->next = temp;
+//     }
+//     return head;
+// }
+
+// void Print(struct Node *head)
+// {
+//     while (head != NULL)
+//     {
+//         printf("%d -> ", head->data);
+//         head = head->next;
+//     }
+//     printf("NULL \n");
+// }
+
+// int main()
+// {
+//     struct Node *head = NULL;
+
+//     head = Insert(head, 20);
+//     head = Insert(head, 50);
+//     head = Insert(head, 250);
+//     head = Insert(head, 70);
+//     head = Insert(head, 90);
+
+//     Print(head);
+// }
+
+
+// Find Middle of a Linked List
+  
+//  struct Node
+//  {
+//     int data;
+//     struct Node *next;
+//  };
+
+//  struct Node *head;
+
+//  struct Node *Insert(struct Node *head,int data){
+//     struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+//     temp -> data = data;
+//     temp -> next = NULL;
+
+//     if(head == NULL){
+//         head = temp;
+//     }
+//     else{
+//         struct Node* node = head;
+//         while (node -> next != NULL )
+//         {
+//             node = node -> next;
+//         }
+//         node -> next = temp;     
+//     }
+//     return head;
+// }
+
+// void print(struct Node *head){
+//      while(head != NULL){
+//         printf("%d -> ",head -> data);
+//         head = head -> next;
+//      }
+//      printf("NULL \n");
+//     }
+
+// void PrintMiddle(struct Node *head){
+//     struct Node *slow = head;
+//     struct Node *fast = head;
+
+//     if(head != NULL){
+//         while (fast != NULL && fast -> next != NULL)
+//         {
+//             slow = slow -> next;
+//             fast = fast -> next -> next;
+//         }
+//         printf("The middle element is %d\n",slow -> data);
+//     }
+//     else{
+//         printf("The linked list is empty\n");
+//     }
+// }
+
+// int main(){
+//       struct Node *head = NULL;
+//         head = Insert(head,20);
+//         head = Insert(head,50);
+//         head = Insert(head,250);
+//         head = Insert(head,70);
+//         head = Insert(head,90);
+
+
+//         print(head);
+//         PrintMiddle(head);
+//         free(head); 
+
+// }
+
+// Reverse a List 
+
 struct Node
 {
     int data;
@@ -43,6 +168,23 @@ void Print(struct Node *head)
     printf("NULL \n");
 }
 
+struct Node *Reverse(struct Node *head){
+    struct Node *curent = head;
+    struct Node *prev = NULL;
+    struct Node *next = NULL;
+
+    while (curent != NULL)
+    {
+        next = curent -> next;
+        curent -> next = prev;
+        prev = curent;
+        curent = next;
+        
+    }
+    head = prev;
+    return head;
+}
+
 int main()
 {
     struct Node *head = NULL;
@@ -53,5 +195,8 @@ int main()
     head = Insert(head, 70);
     head = Insert(head, 90);
 
+    Print(head);
+    printf("Reversing the linked list\n");
+    head = Reverse(head);
     Print(head);
 }
