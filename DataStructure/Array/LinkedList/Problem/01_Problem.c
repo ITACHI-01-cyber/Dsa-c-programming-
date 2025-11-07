108d0c5a34691d5692863ae7cd4846694a7a1650
VIVEK BHARDWAJ <bvivek514@gmail.com>
2025-11-07 16:41:00 +0000

    problem da 02

--- DataStructure/Array/LinkedList/Problem/01_Problem.c
+++ DataStructure/Array/LinkedList/Problem/01_Problem.c
@@ -128,75 +128,185 @@
 
 // Reverse a List 
 
-struct Node
-{
-    int data;
-    struct Node *next;
-};
+// struct Node
+// {
+//     int data;
+//     struct Node *next;
+// };
 
-struct Node *head;
+// struct Node *head;
 
-struct Node *Insert(struct Node *head, int data)
-{
-    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
-    temp->data = data;
-    temp->next = NULL;
+// struct Node *Insert(struct Node *head, int data)
+// {
+//     struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+//     temp->data = data;
+//     temp->next = NULL;
 
-    if (head == NULL)
-    {
-        head = temp;
-    }
-    else
-    {
-        struct Node *node = head;
-        while (node->next != NULL)
-        {
-            node = node->next;
-        }
-        node->next = temp;
-    }
-    return head;
-}
+//     if (head == NULL)
+//     {
+//         head = temp;
+//     }
+//     else
+//     {
+//         struct Node *node = head;
+//         while (node->next != NULL)
+//         {
+//             node = node->next;
+//         }
+//         node->next = temp;
+//     }
+//     return head;
+// }
 
-void Print(struct Node *head)
-{
-    while (head != NULL)
-    {
-        printf("%d -> ", head->data);
-        head = head->next;
-    }
-    printf("NULL \n");
-}
+// void Print(struct Node *head)
+// {
+//     while (head != NULL)
+//     {
+//         printf("%d -> ", head->data);
+//         head = head->next;
+//     }
+//     printf("NULL \n");
+// }
 
-struct Node *Reverse(struct Node *head){
-    struct Node *curent = head;
-    struct Node *prev = NULL;
-    struct Node *next = NULL;
+// struct Node *Reverse(struct Node *head){
+//     struct Node *curent = head;
+//     struct Node *prev = NULL;
+//     struct Node *next = NULL;
 
-    while (curent != NULL)
-    {
-        next = curent -> next;
-        curent -> next = prev;
-        prev = curent;
-        curent = next;
+//     while (curent != NULL)
+//     {
+//         next = curent -> next;
+//         curent -> next = prev;
+//         prev = curent;
+//         curent = next;
         
+//     }
+//     head = prev;
+//     return head;
+// }
+
+// int main()
+// {
+//     struct Node *head = NULL;
+
+//     head = Insert(head, 20);
+//     head = Insert(head, 50);
+//     head = Insert(head, 250);
+//     head = Insert(head, 70);
+//     head = Insert(head, 90);
+
+//     Print(head);
+//     printf("Reversing the linked list\n");
+//     head = Reverse(head);
+//     Print(head);
+// }
+
+//  Delete first, middle, and last nodes
+
+    struct Node
+    {
+        int data;
+        struct Node *next;
+    };
+
+    struct Node *head;
+
+    
+    struct Node *Insert(struct Node *head,int data){
+        struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+
+            temp -> data = data;
+            temp -> next = NULL;
+
+        if (head == NULL)
+        {
+            head = temp;
+        }
+        else{
+            struct Node *node = head;
+
+            while (node != NULL)
+            {
+                node = node -> next;
+            }
+            node -> next = temp;
+        }
+        return head;
     }
-    head = prev;
-    return head;
-}
 
-int main()
-{
-    struct Node *head = NULL;
+    void Print(struct Node*head){
 
-    head = Insert(head, 20);
-    head = Insert(head, 50);
-    head = Insert(head, 250);
-    head = Insert(head, 70);
-    head = Insert(head, 90);
+        while (head != NULL)
+        {
+            printf("%d -> ", head -> data);
+            head = head -> next;
+        }
+        printf("NULL\n");
+    }
 
-    Print(head);
-    printf("Reversing the linked list\n");
-    head = Reverse(head);
-    Print(head);
-}
+    void deletionFirst(struct Node *head){
+        if(head == NULL){
+            return;
+        }
+        struct Node *temp = head;
+        head = head -> next;
+        free(temp);
+        return head;
+    }
+
+    void deletionMiddle(struct Node *head){
+        if(head == NULL){
+            return;
+        }
+        struct Node *slow = head;
+        struct Node *fast = head;
+        struct Node *prev = NULL;
+
+        while (fast != NULL && fast -> next != NULL)
+        {
+            prev = slow;
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        prev -> next = slow -> next;
+        free(slow);
+        return head;
+    }
+
+    void deletionLast(struct Node *head){
+        if(head == NULL){
+            return;
+        }
+        if(head -> next == NULL){
+            free(head);
+            return NULL;
+        }
+        struct Node *temp = head;
+        while (temp -> next -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        free(temp -> next);
+        temp -> next = NULL;
+        return;
+    }
+
+    int main(){
+        struct Node *head = NULL;
+
+        head = Insert(head,20);
+        head = Insert(head,50);
+        head = Insert(head,250);
+        head = Insert(head,70);
+        head = Insert(head,90);
+        printf("Original Linked List:\n");
+        Print(head);
+        head = deletionFirst(head);
+        printf("After deleting first node:\n");
+        Print(head);
+        head = deletionMiddle(head);
+        printf("After deleting middle node:\n");
+        Print(head);
+        printf("After deleting last node:\n");
+        deletionLast(head);
+    }
